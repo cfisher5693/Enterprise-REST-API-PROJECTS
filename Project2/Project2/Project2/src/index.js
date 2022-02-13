179051daef30eaa79a030f06a12e972f39925269
Coleman Fisher <cfisher19@uco.edu>
2022-02-13 13:53:52 -0600

    Git Main commit

---
+++ Project2/Project2/Project2/src/index.js
@@ -0,0 +1,5 @@
+﻿import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/all';
+import './style/site.css';
